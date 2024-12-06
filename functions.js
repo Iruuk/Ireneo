e6e45a7874aae64b53507b648ff4fe7f21902960
Iruuk <irekbezna@gmail.com>
2024-12-06 15:29:28 +0200

    good afternoon

---
+++ functions.js
@@ -0,0 +1,40 @@
+/*dark mode*/
+function myFunction() {
+  document.body.classList.toggle("dark-mode");
+  const allSections = document.querySelectorAll(".main, .sidenav");
+  allSections.forEach(section => section.classList.toggle("dark-mode"));
+}
+/*clock*/
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
+  setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  
+  return i;
+}
+// button:
+let mybutton = document.getElementById("myBtn");
+
+
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
